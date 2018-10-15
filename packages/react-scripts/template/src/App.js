b3d1546c3fbad7a69a9bdca8864532155f32d4bc
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-10-15 08:05:53 +0200

    change app template

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -13,11 +13,11 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://marcopeg.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            marcopeg.com
           </a>
         </header>
       </div>
